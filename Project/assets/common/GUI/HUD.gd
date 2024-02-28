0c270b00574b03bc1c3779fe86a010336a0167cf
Jeff Lee <jungtae@ualberta.ca>
2024-02-27 17:47:34 -0700

    game base

---
+++ Project/assets/common/GUI/HUD.gd
@@ -0,0 +1,15 @@
+extends Control
+
+var LocationInfo : String
+var DateInfo: int
+var TimeInfo: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+# TODO: callbacks for updating InfoHUD contents
